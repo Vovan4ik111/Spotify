9fc373568c6ba9a3f22a5087cf096c6e530180d7
Vovan4ik111 <vovan4ik1981@gmail.com>
2024-07-15 17:55:52 -0400

    Updated sign in
    Added another check from the side menu

--- cypress/e2e/sign_in.cy.js
+++ cypress/e2e/sign_in.cy.js
@@ -10,7 +10,7 @@ describe('SignIn', () => {
         cy.visit('/');
     });
     
-    it('Sign up a new user', () => {
+    it('Sign up a new user from the navbar', () => {
         //Open Login form
         cy.get('.site-nav__link.site-nav__link--icon.small--hide').click();
 
@@ -43,4 +43,43 @@ describe('SignIn', () => {
         //cy.get('.h5').should('have.text', first_name + ' ' + last_name);
 
     });
+
+    it.only('Sign up a new user from the side menu', () => {
+        //Open hamburg menu
+        cy.get('.icon.icon-hamburger').click();
+        
+        //Open Login form
+        cy.get('.mobile-nav__link').contains('Log in').click();
+
+
+        //Verify login URL
+        cy.url().should('include', '/login');
+
+        //Check if it's Login form
+        cy.get('.section-header__title').should('have.text', 'Login');
+
+        const first_name = 'John';
+        const last_name = 'Doe';
+        const email = 'caxev69353@atebin.com'  
+        const password = 'MyPassword2024';
+
+        //Type user's name,last name. email, and password
+        cy.get('#CustomerEmail').type(email, {force: true, delay: 100});
+        cy.get('#CustomerPassword').type(password);
+
+        //Sign In
+        cy.get('button.btn.btn--full').contains('Sign In').click();
+
+        //reCaptcha issue
+        //Open account info
+        //cy.get('.site-nav__link.site-nav__link--icon.small--hide').click();
+
+        //Verify URL for account
+        //cy.url().should('include', '/account');
+        //Verify user name
+        //cy.get('.h5').should('have.text', first_name + ' ' + last_name);
+
+    });
+
+    
 });
